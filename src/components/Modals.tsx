b011cda4a898949f32b30bd1a579657bb635acf3
formegusto <formegusto@gmail.com>
2024-01-21 10:36:18 +0900

    set modal highlight line init

--- src/components/Modals.tsx
+++ src/components/Modals.tsx
@@ -2,5 +2,27 @@ import { useUI } from "@hooks";
 
 export function SetModal() {
   const { mode } = useUI();
-  return mode === "set" ? <div className="set-modal-container"></div> : <></>;
+  return mode === "set" ? (
+    <div className="set-modal-container">
+      <div className="modal-question-group">
+        <span>Your K is</span>
+        <input type="text" maxLength={2} />
+      </div>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 129.53 2"
+        className="highlight-line"
+      >
+        <path
+          d="M 0 1 
+            L 129.53 1 
+            L 129.53 -30
+            L 98.53 -30
+            L 98.53 1"
+        />
+      </svg>
+    </div>
+  ) : (
+    <></>
+  );
 }
