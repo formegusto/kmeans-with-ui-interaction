32848711284689598b7789c8748ab7146fc48784
formegusto <formegusto@gmail.com>
2024-01-28 01:10:34 +0900

    menu open animation

--- src/components/Utils.tsx
+++ src/components/Utils.tsx
@@ -11,11 +11,13 @@ export function UtilsItems() {
         className={`kmeans-utils-btn ${mode === "gen" ? mode : ""}`}
         onClick={
           mode === "gen" ? () => changeMode(null) : () => changeMode("gen")
-        }>
+        }
+      >
         <svg
           xmlns="http://www.w3.org/2000/svg"
           viewBox="0 0 48 48"
-          className="utils-gen">
+          className="utils-gen"
+        >
           <path d="M 23 3 L 23 45" />
           <path d="M 3 24 L 45 24" />
         </svg>
@@ -26,11 +28,13 @@ export function UtilsItems() {
           mode === "set-length"
             ? () => changeMode(null)
             : () => changeMode("set-length")
-        }>
+        }
+      >
         <svg
           xmlns="http://www.w3.org/2000/svg"
           viewBox="0 0 48 48"
-          className="utils-set-length">
+          className="utils-set-length"
+        >
           {/* <path d="M 8 10 L 38 38" />
           <path d="M 38 10 L 8 38" /> */}
           <path d="M 8 10 L 23 24" />
@@ -43,11 +47,13 @@ export function UtilsItems() {
         className={`kmeans-utils-btn ${mode === "set-K" ? mode : ""}`}
         onClick={
           mode === "set-K" ? () => changeMode(null) : () => changeMode("set-K")
-        }>
+        }
+      >
         <svg
           xmlns="http://www.w3.org/2000/svg"
           viewBox="0 0 48 48"
-          className="utils-set-K">
+          className="utils-set-K"
+        >
           <path d="M 23 3 L 23 45" />
           <path d="M 23 3 L 23 45" />
           <path d="M 10 11 C -10 50 56 50 36 11" className="power-round" />
@@ -58,7 +64,8 @@ export function UtilsItems() {
         onClick={() => {
           clearUI();
           clearKMeans();
-        }}>
+        }}
+      >
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48">
           <path d="M 3 9 L 43 9 L 23 9 L 23 1" className="garbage-hat" />
           <path d="M 6 18 C -10 55 56 55 40 18" />
@@ -71,12 +78,14 @@ export function UtilsItems() {
 export function UtilsButton({ isOpen, onClick }: any) {
   return (
     <button
-      className={`kmeans-utils-btn ${isOpen ? "open" : ""}`}
-      onClick={onClick}>
+      className={`kmeans-utils-btn menus ${isOpen ? "open" : ""}`}
+      onClick={onClick}
+    >
       <svg
         xmlns="http://www.w3.org/2000/svg"
         viewBox="0 0 48 48"
-        className="utils-burger">
+        className="utils-burger"
+      >
         <path d="M 3 6 L 45 6" />
         <path d="M 3 22 L 45 22" />
         <path d="M 3 22 L 45 22" />
@@ -91,7 +100,8 @@ export function UtilsArea() {
 
   return (
     <div className="kmeans-utils-area">
-      <UtilsItems />
+      {isOpen && <UtilsItems />}
+
       <UtilsButton
         isOpen={isOpen}
         onClick={isOpen ? () => setIsOpen(false) : () => setIsOpen(true)}
